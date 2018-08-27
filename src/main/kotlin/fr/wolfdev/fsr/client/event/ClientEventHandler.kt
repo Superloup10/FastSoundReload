3f0c20292c664d182ec12c992c472d070aa73a8a
Loup <Superloup10@gmail.com>
2018-08-27 14:23:12 +0200

    Add message

--- src/main/kotlin/fr/wolfdev/fsr/client/event/ClientEventHandler.kt
+++ src/main/kotlin/fr/wolfdev/fsr/client/event/ClientEventHandler.kt
@@ -1,6 +1,11 @@
 package fr.wolfdev.fsr.client.event
 
 import fr.wolfdev.fsr.client.FastSoundReload
+import net.minecraft.client.Minecraft
+import net.minecraft.util.text.Style
+import net.minecraft.util.text.TextComponentString
+import net.minecraft.util.text.TextComponentTranslation
+import net.minecraft.util.text.TextFormatting
 import net.minecraftforge.client.resource.VanillaResourceType
 import net.minecraftforge.fml.client.FMLClientHandler
 import net.minecraftforge.fml.common.Mod
@@ -13,7 +18,14 @@ object ClientEventHandler {
     @JvmStatic
     @SubscribeEvent
     fun keyInput(event: InputEvent.KeyInputEvent) {
-        if (FastSoundReload.SOUND_KEY.isPressed)
+        if (FastSoundReload.SOUND_KEY.isPressed) {
             FMLClientHandler.instance().refreshResources(VanillaResourceType.SOUNDS)
+            FMLClientHandler.instance().client.debugFeedbackTranslated("debug.reload_sound.message")
+        }
+    }
+
+    @JvmStatic
+    fun Minecraft.debugFeedbackTranslated(untranslatedTemplate: String) {
+        this.ingameGUI.chatGUI.printChatMessage(TextComponentString("").appendSibling(TextComponentTranslation("debug.prefix").setStyle(Style().setColor(TextFormatting.YELLOW).setBold(true))).appendText(" ").appendSibling(TextComponentTranslation(untranslatedTemplate)))
     }
 }
